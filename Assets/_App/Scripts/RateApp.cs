dfd60ae862ea713fc1375d824282627175aeb38e
chuong_gamer <hieutg244@gmail.com>
2023-10-04 00:14:21 +0700

    update

--- Assets/_App/Scripts/RateApp.cs
+++ Assets/_App/Scripts/RateApp.cs
@@ -1,13 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RateApp : MonoBehaviour
 {
+    public GameObject GameObject;
+
+    private void Start()
+    {
+        GameObject.SetActive(PlayerPrefs.GetInt("Rate", 0) == 0);
+    }
+
     public void OnClickRateUs()
     {
         Application.OpenURL("market://details?id=" + Application.identifier);
+        PlayerPrefs.SetInt("Rate", 1);
     }
+
     public void OnClickPrivacy()
     {
         Application.OpenURL("");
